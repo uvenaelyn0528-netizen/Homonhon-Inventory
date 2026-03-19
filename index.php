fd1a2957217ac4720a3db1c80af33173e1f07a59
uvenaelyn0528-netizen <uvenaelyn0528@gmail.com>
2026-03-19 08:08:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
 include 'db.php'; 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Capture the role from session
 $role = $_SESSION['role'] ?? 'Viewer'; 
