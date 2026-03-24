dfc12fd81d472f8ff9536e61a241226588fdbed2
uvenaelyn0528-netizen <uvenaelyn0528@gmail.com>
2026-03-24 18:18:46 +0800

    Update diesel_inventory.php

--- diesel_inventory.php
+++ diesel_inventory.php
@@ -260,7 +260,7 @@ $tanks_ft = ["Tank 1", "Tank 2", "Tank 3", "Tank 4", "Tank 5", "Tank 6", "Tank 7
                     <td>
                         <?= htmlspecialchars($row['rr_no'] ?: '---') ?>
                         <?php if (!empty($row['attachment_path'])): ?>
-                            <a href="<?= htmlspecialchars($row['attachment_path']) ?>" target="_blank" class="attachment-link" title="View Attachment">📎</a>
+                            <a href="<?= htmlspecialchars($row['attachment_path']) ?>" target="_blank" class="attachment-link" title="View Attachment">📎 PDF File</a>
                         <?php endif; ?>
                     </td>
                     <td><?= htmlspecialchars($row['ws_no'] ?: '---') ?></td>
